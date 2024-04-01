bc951ec6e7cd8be7b2ef1ffe3679cb6d61a958cc
Дмитрий <144824018+osm1us@users.noreply.github.com>
2024-04-01 17:53:43 +0300

    Add files via upload

---
+++ Alg & Prog/2 sem/19_03_2024_1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication34
+{
+    class Program
+    {
+        public delegate double UnaryOperation(double a);
+        public delegate double BinaryOperation(double a, double b);
+
+        static double ProcessUnaryOperation(UnaryOperation op)
+        {
+            Console.WriteLine("Введите число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            return op(a);
+        }
+        static double ProcessBinaryOperation(BinaryOperation op)
+        {
+            Console.WriteLine("Введите первый операнд");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второй операнд");
+            double b = Convert.ToDouble(Console.ReadLine());
+            
+            return op(a, b);
+        }
+        static double SafeDivision(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Деление на ноль");
+            }
+            return a / b;
+        }
+        static double SafeSqrt(double a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Корень из отрицательного числа");
+            }
+            return Math.Sqrt(a);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Какую операцию выполнить? (сложение, вычитание, умножение, деление, корень, синус, косинус)");
+            string answer = Console.ReadLine();
+            double result = 0;
+            switch (answer) {
+                case "сложение":
+                    result = ProcessBinaryOperation((double a, double b) => { return a + b; });
+                    break;
+                case "вычитание":
+                    result = ProcessBinaryOperation((double a, double b) => { return a - b; });
+                    break;
+                case "умножение":
+                    result = ProcessBinaryOperation((double a, double b) => { return a * b; });
+                    break;
+                case "деление":
+                    result = ProcessBinaryOperation(SafeDivision);
+                    break;
+                case "корень":
+                    result = ProcessUnaryOperation(SafeSqrt);
+                    break;
+                case "синус":
+                    result = ProcessUnaryOperation(Math.Sin);
+                    break;
+                case "косинус":
+                    result = ProcessUnaryOperation(Math.Cos);
+                    break;
+            }
+            
+            
+            Console.WriteLine("Результат: {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Alg & Prog/2 sem/19_03_2024_2.cs
@@ -0,0 +1,46 @@
+﻿class Car
+{
+    public string name;
+
+    public Car(string name)
+    {
+        this.name = name;
+    }
+}
+class Garage
+{
+    List<Car> cars;
+
+    public Garage(List<Car> cars)
+    {
+        this.cars = cars;
+    }
+
+    public delegate void CarWashDelegate(Car car);
+    public void WashAllCars(CarWashDelegate washCar)
+    {
+        foreach (Car car in cars)
+        {
+            washCar(car);
+        }
+    }
+}
+class CarWasher
+{
+    public void WashCarInCarWasher(Car car)
+    {
+        Console.WriteLine("Машина \"{0}\" помыта", car.name);
+    }
+}
+class Program
+{
+    
+    public static void Main()
+    {
+        List<Car> cars = new List<Car>() { new Car("ВАЗ 2110"), new Car("Москвич 412"), new Car("УАЗ 3151"), new Car("ВАЗ 1111 Ока") };
+        Garage garage = new Garage(cars);
+        CarWasher carWasher = new CarWasher();
+        garage.WashAllCars(carWasher.WashCarInCarWasher);
+        Console.ReadKey();
+    }
+}
