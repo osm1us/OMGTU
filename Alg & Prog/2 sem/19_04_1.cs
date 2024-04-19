cff133d2c3d23b244389ca5aeceb09f088d8a6c7
Дмитрий <144824018+osm1us@users.noreply.github.com>
2024-04-19 14:17:11 +0600

    Add files via upload

---
+++ Alg & Prog/2 sem/19_04_1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\Documents\files\";
+            string path_file_1 = path + "file_1.txt";
+            string path_file_2 = path + "file_2.txt";
+            string path_result_file = path + "merged_1_2.txt";
+
+            using (StreamReader reader_1 = new StreamReader(path_file_1))
+            {
+                using (StreamReader reader_2 = new StreamReader(path_file_2))
+                {
+                    using (StreamWriter writer = new StreamWriter(path_result_file, true))
+                    {
+                        int line_1 = Convert.ToInt32(reader_1.ReadLine());
+                        
+                        int line_2 = Convert.ToInt32(reader_2.ReadLine());
+                        int current = 0;
+                        while (line_1 != 0 || line_2 != 0)
+                        {
+                            if (line_2 == 0 || line_1 < line_2)
+                            {
+                                current = line_1;
+                                line_1 = Convert.ToInt32(reader_1.ReadLine());
+                            }
+                            else if (line_1 == 0 || line_2 < line_1) 
+                            {
+                                current = line_2;
+                                line_2 = Convert.ToInt32(reader_2.ReadLine());
+                            }
+                            writer.WriteLine(Convert.ToString(current));
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Alg & Prog/2 sem/19_04_2024_2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\Documents\files\strings.txt";
+            string min_string = "";
+            int min_count = int.MaxValue;
+            foreach (var st in File.ReadLines(path)) {
+                var matches = Regex.Matches(st, @"a+");
+                string[] substrings = new string[matches.Count];
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    substrings[i] = matches[i].ToString();
+                }
+                int min_sub;
+                if (substrings.Length == 0)
+                {
+                    min_sub = 0;
+                }
+                else 
+                {
+                    min_sub = substrings.Min(str => str.Length);   
+                }
+                
+                if (min_sub < min_count)
+                {
+                    min_string = st;
+                    min_count = min_sub;
+                }
+            }
+            Console.WriteLine(min_string);
+            Console.WriteLine(min_count);
+            Console.ReadKey();
+        }
+    }
+}
