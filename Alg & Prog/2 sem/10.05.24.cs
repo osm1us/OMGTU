406c6bd197bce80fb993e211eef626faa8c35126
Дмитрий <144824018+osm1us@users.noreply.github.com>
2024-05-20 19:44:40 +0600

    Add files via upload

---
+++ Alg & Prog/2 sem/10.05.24.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.ComponentModel;
+namespace ConsoleApplication5
+{
+    class Product
+    {
+        public int number;
+        public string name;
+        public string category;
+        public int count;
+        public double price;
+        public string stock;
+        public Product(int number, string name, string category, int count, double price, string stock)
+        {
+            this.number = number;
+            this.name = name;
+            this.category = category;
+            this.count = count;
+            this.price = price;
+            this.stock = stock;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Product cheese = new Product(1, "cheese", "milk product", 20, 299, "sklad 1");
+            Product beer = new Product(2, "beer", "alcohol", 44, 89, "sklad 2");
+            Product mayonaise = new Product(3, "Mayonaise", "milk product", 59, 344, "sklad 1");
+            Product cucumber = new Product(20, "Cucumber", "Groceries", 100, 50, "sklad 3");
+            Product whiskey = new Product(44, "Whiskey", "alcohol", 10, 1000, "sklad 2");
+            Product missingn = new Product(4, "??????", "none", 10, 10000, "sklad 4");
+            Product fridge = new Product(145, "Fridge", "none", 1, 50000, "sklad 4");
+            List<Product> list = new List<Product>() { cheese, beer, mayonaise, cucumber, whiskey, missingn, fridge };
+            var amount = from l in list group l by l.stock into g select new { Stock = g.Key, Count = g.Sum(p => p.count) };
+            foreach (var item in amount)
+            {
+                Console.WriteLine(item.Stock + " " + item.Count);
+            }
+            Console.WriteLine("===============");
+            var maxincat = from l in list group l by l.category into g select new { Category = g.Key, MaxPrice = g.Max(p => p.price) };
+            foreach(var item in maxincat)
+            {
+                Console.WriteLine(item.Category + " " + item.MaxPrice);
+            }
+            Console.WriteLine("===============");
+            double noneaverage = list.Where(l => l.category == "none").Average(l => l.price);
+            Console.WriteLine(noneaverage);
+            Console.WriteLine("===============");
+            var cheapest = from l in list where l.price == list.Min(l=>l.price) select l;
+            foreach(var item in cheapest)
+            {
+                Console.WriteLine(item.name);
+            }
+            Console.WriteLine("===============");
+            double everything = list.Sum(l => l.price * l.count);
+            Console.WriteLine(everything);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Alg & Prog/2 sem/14.05.24.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+namespace lul
+{
+    class Worker
+    {
+        public int number;
+        public string name;
+        public string education;
+        public string job;
+        public double salary;
+        public int amount;
+        public double price;
+        public Worker(int number, string name, string education, string job, double salary, int amount, double price)
+        {
+            this.number = number;
+            this.name = name;
+            this.education = education;
+            this.job = job;
+            this.salary = salary;
+            this.amount = amount;
+            this.price = price;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Worker oleg = new Worker(1, "Никотинов Олег Геннадьевич", "2 высших", "Слесарь", 100000, 1000, 50);
+            Worker sergei = new Worker(2, "Бумажкин Сергей Константинович", "8 классов", "Фрилансер", 100000, 5000, 1000);
+            Worker dmitrii = new Worker(3, "Матвеев Дмитрий Андреевич", "Среднее образование", "СММ", 90000, 1000, 1000);
+            Worker ilya = new Worker(4, "Коровкин Илья Сулейманович", "2 высших", "Фрезеровщик", 30000, 1000, 200);
+            Worker David = new Worker(5, "Тильт Давид", "4.5 класса", "Курьер", 11250, 100, 100);
+            List<Worker> list = new List<Worker>() { oleg, sergei, dmitrii, ilya, David };
+            var lessthanproducts = from l in list where l.salary < l.amount*l.price select l;
+            foreach ( var l in lessthanproducts)
+            {
+                Console.WriteLine(l.name);
+            }
+            Console.WriteLine("===============");
+            var overallamount = from l in list group l by l.name into g select new { Name = g.Key, Amount = g.Sum(l => l.amount) };
+            foreach ( var l in overallamount)
+            {
+                Console.WriteLine(l.Name + " " + l.Amount);
+            }
+            Console.WriteLine("===============");
+            double overallprice = list.Sum(l=>l.amount * l.price) ;
+            Console.WriteLine(overallprice);
+            Console.WriteLine("===============");
+            int num = list.Count(l=> 0.5 *l.salary >= l.amount * l.price);
+            Console.WriteLine(num);
+            Console.ReadKey(); 
+        }
+    }
+}
+
+       
